89e6cd4d99591b0a6ae7766ae54c4768e7e30ccd
mantagen <mathsgregory@gmail.com>
2015-09-23 19:22:55 +0100

    displaying random image

--- instagram.js
+++ instagram.js
@@ -1,6 +1,6 @@
 
 //var test_images = JSON.parse("response.json");
-//var randomImageNum = Math.floor(Math.random()*20);
-//var randomImageUrl = test_images.data[randomImageNum].images.standard_resolution.url;
+var randomImageNum = Math.floor(Math.random()*20);
+var randomImageUrl = test_images.data[randomImageNum].images.standard_resolution.url;
 
-//document.getElementById('main-ground-container').innerHTML='<div style="width:100%;height:100%;background-image:url('+randomImageUrl+'); background-size:cover; background-position:center;"></div>';
+document.getElementById('main-ground-container').innerHTML='<div style="width:100%;height:100%;background-image:url('+randomImageUrl+'); background-size:cover; background-position:center;"></div>';
