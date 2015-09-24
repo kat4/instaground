764b09c4868c7d9ca82065f01f35d16b23b5d1c8
mantagen <mathsgregory@gmail.com>
2015-09-24 19:35:21 +0100

    wip

--- instagram.js
+++ instagram.js
@@ -48,7 +48,7 @@ var instaground = (function() {
       var imageUrl = clientHistory[i];
       var oldImage = '<div class="history-image"><img src="'+imageUrl+'" /></div>';
       var historyDiv = document.getElementById('history-content');
-      oldImage = historyDiv.innerHTML + oldImage;
+      oldImage = oldImage + historyDiv.innerHTML;
       historyDiv.innerHTML = oldImage;
     }
 
