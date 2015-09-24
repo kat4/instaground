b5da3b721ace2cdd75e0a06ca2c428573a8e0a09
katkelemen <kelemenkata@gmail.com>
2015-09-24 10:03:21 +0100

    create separate .ls and .css files

--- instagram.js
+++ instagram.js
@@ -1,6 +1,35 @@
+var newAccessKey = window.location.hash.slice(1);
+//
+// var newURL = "https://api.instagram.com/v1/media/popular?" + newAccessKey;
+//     function httpGetAsync(theUrl, callback) {
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function() {
+//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+//             callback(xmlHttp.responseText);
+//     }
+//     xmlHttp.open("GET", theUrl, true); // true for asynchronous
+//     xmlHttp.send(null);
+// }
+//
+//   httpGetAsync(newURL, function() {console.log("Succes")})
 
-//var test_images = JSON.parse("response.json");
-//var randomImageNum = Math.floor(Math.random()*20);
-//var randomImageUrl = test_images.data[randomImageNum].images.standard_resolution.url;
+function jsonp(callback) {
+  //var searchterm = document.getElementById('searchTermInput').value.toString() || document.getElementById('searchTermInput').placeholder;
+  var url = 'https://api.instagram.com/v1/tags/' + 'autumn' + '/media/recent?' + newAccessKey;
+  var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+  window[callbackName] = function(data) {
+    delete window[callbackName];
+    document.body.removeChild(script);
+    callback(data);
+    console.log(data)
+  };
 
-//document.getElementById('main-ground-container').innerHTML='<div style="width:100%;height:100%;background-image:url('+randomImageUrl+'); background-size:cover; background-position:center;"></div>';
+  var script = document.createElement('script');
+  script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+  document.body.appendChild(script);
+}
+
+
+jsonp(function() {
+  console.log('Success')
+})
