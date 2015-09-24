8d473374aa210d21648db0c77bc5773a36be8e81
mantagen <mathsgregory@gmail.com>
2015-09-24 16:19:20 +0100

    Encapsulating javascript

--- instagram.js
+++ instagram.js
@@ -1,55 +1,54 @@
-var newAccessKey = window.location.hash.slice(1);
-if (newAccessKey.slice(0,13) !== "access_token="){
-window.location = "https://instagram.com/oauth/authorize/?client_id=d80c733cf6c9474e8df5daffc9a9a2b7&redirect_uri=http://kat4.github.io/instaground&response_type=token";
-}
-//
-// var newURL = "https://api.instagram.com/v1/media/popular?" + newAccessKey;
-//     function httpGetAsync(theUrl, callback) {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.onreadystatechange = function() {
-//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-//             callback(xmlHttp.responseText);
-//     }
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous
-//     xmlHttp.send(null);
-// }
-//
-//   httpGetAsync(newURL, function() {console.log("Succes")})
+var iGround = (function() {
+  "use strict";
 
+  // Check whether user is logged into instagram
 
-function jsonp(callback) {
-  var searchTag = document.getElementById('search-field').value;
+  var newAccessKey = window.location.hash.slice(1);
+  if (newAccessKey.slice(0, 13) !== "access_token=") {
+    window.location = "https://instagram.com/oauth/authorize/?client_id=d80c733cf6c9474e8df5daffc9a9a2b7&redirect_uri=http://kat4.github.io/instaground&response_type=token";
+  }
 
-  var url = 'https://api.instagram.com/v1/tags/' + searchTag + '/media/recent?' + newAccessKey;
-  var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-  window[callbackName] = function(data) {
-    delete window[callbackName];
-    document.body.removeChild(script);
-    callback(data);
-    console.log(data);
+  // Request images from instagram
+  function jsonp(callback) {
+    var searchTag = document.getElementById('search-field').value;
+
+    var url = 'https://api.instagram.com/v1/tags/' + searchTag + '/media/recent?' + newAccessKey;
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    window[callbackName] = function(data) {
+      delete window[callbackName];
+      document.body.removeChild(script);
+      callback(data);
+      console.log(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+  }
+
+  // Callback function
+  function displayRandomImage(response) {
+    var randomImageNum = Math.floor(Math.random() * 20);
+    var randomImageUrl = response.data[randomImageNum].images.standard_resolution.url;
+    document.getElementById('background-container').style.backgroundImage = 'url("' + randomImageUrl + '")';
+  }
+
+  return {
+    jsonp: jsonp,
+    displayRandomImage: displayRandomImage
   };
 
-  var script = document.createElement('script');
-  script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-  document.body.appendChild(script);
-}
+});
 
-function displayRandomImage(response) {
-  var randomImageNum = Math.floor(Math.random() * 20);
-  var randomImageUrl = response.data[randomImageNum].images.standard_resolution.url;
-  document.getElementById('background-container').style.backgroundImage = 'url("' + randomImageUrl + '")';
-}
-
+// Search on click
 var searchButton = document.getElementById('search-button');
-searchButton.addEventListener('onclick', function() { jsonp(displayRandomImage);});
-
+searchButton.addEventListener('onclick', function() {
+  iGround.jsonp(displayRandomImage);
+});
+// Search on enter key
 var searchField = document.getElementById('search-field');
 searchField.addEventListener('keydown', function(e) {
-  if(e.keyCode === 13) {
-    jsonp(displayRandomImage);
+  if (e.keyCode === 13) {
+    iGround.jsonp(displayRandomImage);
   }
-}
-);
-// get key presses only when the textfield is being edited
-
-//searchField.addEventListener(KeyboardEvent.KEY_DOWN, handler);
+});
